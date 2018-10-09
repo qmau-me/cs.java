0c412385fb2c36e0e1bbb671d15e16a97c4b664a
Pratik Raj <Rajpratik71@gmail.com>
2018-10-09 10:46:32 +0530

    Automatically resizing vector implementation
    Automatically resizing vector implementation in java

---
+++ src/me/qmau/cs/java/dataStructure/AutomaticallyResizingVector.java
@@ -0,0 +1,17 @@
+/**
+ * Increases the capacity to ensure that it can hold at least the
+ * number of elements specified by the minimum capacity argument.
+ *
+ * @param minCapacity the desired minimum capacity
+ */
+private void grow(int minCapacity) {
+    // overflow-conscious code
+    int oldCapacity = elementData.length;
+    int newCapacity = oldCapacity + (oldCapacity >> 1);
+    if (newCapacity - minCapacity < 0)
+        newCapacity = minCapacity;
+    if (newCapacity - MAX_ARRAY_SIZE > 0)
+        newCapacity = hugeCapacity(minCapacity);
+    // minCapacity is usually close to size, so this is a win:
+    elementData = Arrays.copyOf(elementData, newCapacity);
+}
